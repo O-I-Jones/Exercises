94c7adb21963868611fdb62a6556a3299d823086
O-I-Jones <83507527+O-I-Jones@users.noreply.github.com>
2023-02-01 16:51:06 +0000

    Exercise 6

---
+++ Exercises/Ex6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    internal class Ex6
+    {
+        public static void Exercise6(string input)
+        {
+            char[] characters = input.ToArray();
+            char[] reverseChars = characters; 
+            Array.Reverse(reverseChars);
+            string reversed = new string(reverseChars);
+            if (input == reversed)
+            {
+                Console.WriteLine("\"" + input + "\" is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine("\"" + input + "\" is not a palindrome");
+            }
+        }
+    }
+}
--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -13,17 +13,22 @@ namespace Exercises
             Console.WriteLine("Runing Exercise 1...");
             Ex1.Exercise1("1,2,1,1,0,3,1,0,0,2,4,1,0,0,0,0,2,1,0,3,1,0,0,0,6,1,3,0,0,0");
 
-            Console.WriteLine("Runing Exercise 2...");
+            Console.WriteLine("\nRuning Exercise 2...");
             Ex2.Exercise2("parts,traps,arts,rats,starts,tarts,rat,art,tar,tars,stars,stray");
 
-            Console.WriteLine("Runing Exercise 3...");
+            Console.WriteLine("\nRuning Exercise 3...");
             Ex3.Exercise3(5);
 
-            Console.WriteLine("Runing Exercise 4...");
+            Console.WriteLine("\nRuning Exercise 4...");
             Ex4.Exercise4(5);
 
-            Console.WriteLine("Runing Exercise 5...");
+            Console.WriteLine("\nRuning Exercise 5...");
             Ex5.Exercise5("hello world");
+
+            Console.WriteLine("\nRuning Exercise 6...");
+            Ex6.Exercise6("madam");
+            Ex6.Exercise6("step on no pets");
+            Ex6.Exercise6("book");
 
         }
     }
